321c6ba348d418d6df8e0eab9a50f3a8e4ed2121
KoKoLates <the21515@gmail.com>
2025-06-20 20:31:25 +0800

    feat(example): update example for toolchain testing

--- example/main.cpp
+++ example/main.cpp
@@ -1,7 +1,31 @@
 #include <string>
 #include <iostream>
+#include <sys/utsname.h>
 
-int main(int argc, char** argv) {
-    std::cout << "Hello, World" << std::endl;
+
+int main(int argc, char **argv) {
+
+#ifdef __aarch64__
+    std::cout << "[INFO] Running on AArch64." << std::endl;
+#elif defined(__arm__)
+    std::cout << "[INFO] Running on ARM (ARMv7 or older)." << std::endl;
+#elif defined(__x86_64__)
+    std::cout << "[INFO] Running on x86_64." << std::endl;
+#else
+    std::cout << "[Warning] Running on unkown architecture." << std::endl;
+#endif
+
+    struct utsname sys_info;
+    if (uname(&sys_info) == 0) {
+        std::cout << "[INFO] System Name: " << sys_info.sysname  << std::endl;
+        std::cout << "[INFO] Node Name: "   << sys_info.nodename << std::endl;
+        std::cout << "[INFO] Release: "     << sys_info.release  << std::endl;
+        std::cout << "[INFO] Version: "     << sys_info.version  << std::endl;
+        std::cout << "[INFO] Machine: "     << sys_info.machine  << std::endl;
+    }
+    else {
+        std::cerr << "[ERROR] Cannot obtain systems information with uname." << std::endl;
+    }
+
     return 0;
 }
